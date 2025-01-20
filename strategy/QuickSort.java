0bd8970d2e5b7cf5564b8685299908e3e523d994
BrysonS1014 <bryson.i.sinclair@gmail.com>
2025-01-19 20:50:03 -0500

    Sorting Algorithm

--- strategy/QuickSort.java
+++ strategy/QuickSort.java
@@ -1,12 +1,19 @@
 import java.util.ArrayList;
 import java.util.Collections;
-
+/**
+ * Implementation of QuickSort
+ */
 public class QuickSort implements SortBehavior {
     @Override
     public void sort(ArrayList<Appartment> appartments) {
         quickSort(appartments, 0, appartments.size() - 1);
     }
-
+    /**
+     * Recursively sorts the subarrays
+     * @param appartments List of apparments
+     * @param low Starting index
+     * @param high Ending index
+     */
     private void quickSort(ArrayList<Appartment> appartments, int low, int high) {
         if (low < high) {
             int pi = partition(appartments, low, high);
@@ -14,7 +21,13 @@ public class QuickSort implements SortBehavior {
             quickSort(appartments, pi + 1, high);
         }
     }
-
+    /**
+     * Partitions the array for QuickSort
+     * @param appartments List of appartments
+     * @param low Starting Index
+     * @param high Ending Index
+     * @return Partition Index
+     */
     private int partition(ArrayList<Appartment> appartments, int low, int high) {
         Appartment pivot = appartments.get(high);
         int i = low - 1;
