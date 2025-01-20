3acda7ecf1fd61d09a36e9796dff6f9a80b58eac
BrysonS1014 <bryson.i.sinclair@gmail.com>
2025-01-19 20:46:09 -0500

    Appartment Listing and Sorting

--- strategy/AppartmentList.java
+++ strategy/AppartmentList.java
@@ -1,22 +1,42 @@
 import java.util.ArrayList;
-
+/**
+ * Manages a list of Appartments and sorts 
+ */
 public class AppartmentList {
     private ArrayList<Appartment> appartments;
     private SortBehavior sortBehavior;
 
+    /**
+     * Constructor initializes the apartment list with a default sorting behavior.
+     */
     public AppartmentList() {
         appartments = new ArrayList<>();
         sortBehavior = new BubbleSort(); // Default sorting behavior (highest to lowest)
     }
 
+    /**
+     * Adds a new appartment to the list
+     * @param address Appartment address
+     * @param numBedrooms Number of bedrooms
+     * @param numBathrooms Number of bathrooms
+     * @param price Price of the Appartment
+     */
     public void add(String address, int numBedrooms, int numBathrooms, double price) {
         appartments.add(new Appartment(address, numBedrooms, numBathrooms, price));
     }
 
+    /**
+     * Sets the sort behavior to be used
+     * @param sortBehavior Sort to be applied
+     */
     public void setSortBehavior(SortBehavior sortBehavior) {
         this.sortBehavior = sortBehavior;
     }
 
+    /**
+     * Returns a sorted list of Appartments based on the sorting strategy
+     * @return Sorted List of Appartments
+     */
     public ArrayList<Appartment> getSortedList() {
         ArrayList<Appartment> sortedList = new ArrayList<>(appartments);
         sortBehavior.sort(sortedList);
