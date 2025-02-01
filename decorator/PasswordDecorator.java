09b7df99fdc057c6b769b98397ce34aecaa53949
BrysonS1014 <bryson.i.sinclair@gmail.com>
2025-02-01 17:36:11 -0500

    Added Javadoc comments

--- decorator/PasswordDecorator.java
+++ decorator/PasswordDecorator.java
@@ -1,13 +1,23 @@
+/**
+ * Abstract class to decorate the password
+ */
 public abstract class PasswordDecorator extends Password{
 
     protected Password passwordBeginning;
 
+    /**
+     * Creates the base password
+     * @param passwordBeginning base Password
+     */
     public PasswordDecorator(Password passwordBeginning) {
 
         this.passwordBeginning = passwordBeginning;
 
     }
     
+    /**
+     * Method to return the decorated password
+     */
     @Override
     public abstract String getPassword();
 }
