ad22e7013d010efb520e61f327f6cf27bc7ebe7d
BrysonS1014 <bryson.i.sinclair@gmail.com>
2025-02-01 17:35:34 -0500

    Decorates Password

---
+++ decorator/PasswordDecorator.java
@@ -0,0 +1,13 @@
+public abstract class PasswordDecorator extends Password{
+
+    protected Password passwordBeginning;
+
+    public PasswordDecorator(Password passwordBeginning) {
+
+        this.passwordBeginning = passwordBeginning;
+
+    }
+    
+    @Override
+    public abstract String getPassword();
+}
