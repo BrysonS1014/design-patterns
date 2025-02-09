1f3cffda29f7973ad6b72f1b278f724817573881
BrysonS1014 <bryson.i.sinclair@gmail.com>
2025-02-09 17:15:59 -0500

    English MusicBox

---
+++ state/EnglishState.java
@@ -0,0 +1,43 @@
+
+/**
+ * English State to play the English Lyrics of Twinkle and Happy
+ */
+public class EnglishState extends State {
+    private static final String TWINKLE_FILE_NAME = "twinkle-english.txt";
+    private static final String HAPPY_FILE_NAME = "happy-english.txt";
+
+    /**
+     * Constructs MusicBox with the English State
+     * @param box State of the music box
+     */
+    public EnglishState(MusicBox box) {
+        super(box, TWINKLE_FILE_NAME, HAPPY_FILE_NAME);
+    }
+
+    /**
+     * Scenario if the English State is pressed
+     */
+    @Override
+    public void pressEnglishButton() {
+        System.out.println("Already in English State");
+    }
+
+    /**
+     * Switches to Spanish State
+     */
+    @Override
+    public void pressSpanishButton() {
+        System.out.println("Switching to Spanish State");
+        box.setState(box.getSpanishState());
+    }
+
+    /**
+     * Switches to French State
+     */
+    @Override
+    public void pressFrenchButton() {
+        System.out.println("Switching to French State");
+        box.setState(box.getFrenchState());
+    }
+
+}
